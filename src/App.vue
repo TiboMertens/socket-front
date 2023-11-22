e1bb6316d1feeeb8f3bca37fb6fba7aa21d26ef5
TiboMertens <tibomertens25@gmail.com>
2023-11-22 16:25:18 +0100

    Add initial project files

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Chat from './components/Chat.vue';
+
+</script>
+
+<template>
+  <div>
+    <h1>Discord</h1>
+    <Chat />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Chat.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, reactive, onMounted } from 'vue';
+
+let messages = ref(['yo', 'sup']);
+let newMessage = ref('');
+let socketServer = null;
+
+onMounted(() => {
+    socketServer = new WebSocket('ws://localhost:3000/primus');
+    
+    //listen for messages
+    socketServer.onmessage = function (event) {
+        let data = JSON.parse(event.data);
+        if (data.action === 'newMessage') {
+            messages.value.push(data.message);
+        }
+    };
+});
+
+const sendMessage = () => {
+    let m = {
+        "action": "newMessage",
+        "message": newMessage.value
+    };
+
+    socketServer.send(JSON.stringify(m));
+};
+</script>
+
+<template>
+    <div>
+        <h1>Chat</h1>
+        <ul>
+            <li v-for="m in messages">{{ m }}</li>
+        </ul>
+        <div><input type="text" v-model="newMessage"></div>
+        <button @click="sendMessage">Send</button>
+    </div>
+</template>
+
+<style scoped></style>
